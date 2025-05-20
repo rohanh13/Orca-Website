0a16b8ad3b44851fb979aaad298d65029d71d8de
rohanh13 <rohanhundal13@gmail.com>
2025-05-20 13:02:11 -0700

    Update 5.0

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let lastScrollTop = 0;
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function () {
+  const currentScroll = window.scrollY;
+
+  if (window.innerWidth <= 600) { // mobile only
+    if (currentScroll > lastScrollTop) {
+      // scrolling down
+      navbar.style.top = '-200px';
+    } else {
+      // scrolling up
+      navbar.style.top = '0';
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  } else {
+    // Always show on desktop
+    navbar.style.top = '0';
+  }
+});
+  
 document.querySelector('h1').addEventListener('click', function() {
   location.reload();
 });
